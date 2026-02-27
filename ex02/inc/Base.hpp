cafa08f490ab3f00a084184b92e52920e8ecba1d
Killian Barru <shaper.vaquero-03@icloud.com>
2026-02-27 13:53:38 +0100

    set identify and generate as functions instead of methods

--- ex02/inc/Base.hpp
+++ ex02/inc/Base.hpp
@@ -17,10 +17,6 @@ class Base {
 public:
 	Base();
 	virtual ~Base() {}
-	static Base *generate(void);
-	static void identify(Base* p);
-	static void identify(Base& p);
-	
 };
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -11,11 +11,69 @@
 /* ************************************************************************** */
 
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-int main() {
-	Base *base = Base::generate();
-	Base::identify(base);
-	Base::identify(*base);
+Base *generate(void) {
+	std::srand(std::time(NULL));
+	int random = std::rand() % 3;
+	switch (random) {
+		case 0:
+			std::cout << "Generated type: A" << std::endl;
+			return new A();
+		case 1:
+			std::cout << "Generated type: B" << std::endl;
+			return new B();
+		case 2:
+			std::cout << "Generated type: C" << std::endl;
+			return new C();
+		default:
+			std::cout << "Failed to generate type" << std::endl;
+			return NULL;
+	}
+}
+
+void identify(Base* p) {
+	std::cout << "Identifying type by pointer: ";
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown" << std::endl;
+}
+
+void identify(Base& p) {
+	std::cout << "Identifying type by reference: ";
+	try {
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	} catch (std::exception&) {
+		try {
+			(void)dynamic_cast<B&>(p);
+			std::cout << "B" << std::endl;
+		} catch (std::exception&) {
+			try {
+				(void)dynamic_cast<C&>(p);
+				std::cout << "C" << std::endl;
+			} catch (std::exception&) {
+				std::cout << "Unknown" << std::endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	Base *base = generate();
+	identify(base);
+	identify(*base);
 	delete base;
 	return 0;
 }
